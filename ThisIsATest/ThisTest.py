04cacdcc8ffe1908633640bc2794f6142a7e5f97
de Jong Aurélie <F80860485@agsad.admin.ch>
2022-03-28 11:14:35 +0200

    commit

---
+++ ThisIsATest/ThisTest.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def function(a):
+    b = a + 2
+    return b
+
+if __name__ == "__main__":
+    print('************* \n Call function')
+    b = function(2)
+    print('result', b)
+    
